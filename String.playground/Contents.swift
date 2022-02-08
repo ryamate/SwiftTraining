73f28a710514ec5355f0357cb7670e0f5d5a5c05
Ryuzo Yamate <iiiiiii.satenanbon@gmail.com>
2022-02-08 16:19:01 +0900

    16. 文字列

---
+++ String.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+let a = "Hello "
+let b = "World!"
+
+let c = a + b
+print(c)
+
+
+let d = "私は"
+let e = "歳です"
+let f = 20
+
+let g = d + "\(f)" + e
+print(g)
