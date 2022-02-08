6b971d3c93a6a6b2bdbafd50b04bb84f206195aa
Ryuzo Yamate <iiiiiii.satenanbon@gmail.com>
2022-02-08 16:38:26 +0900

    18. 分岐

---
+++ Branch.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+let a = true
+
+if a {
+    print("Hello!")
+}else{
+    print("Hi!")
+}
+
+let time = 21
+
+if time < 12 {
+    print("Good mornig!")
+}else if time < 18 {
+    print("Good afternoon!")
+}else{
+    print("Good night!")
+}
